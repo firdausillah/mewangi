e0313702d4e3ad79f7057a55e2df8ea2c36a30e5
firdausillah <firdausillah123@gmail.com>
2024-10-26 18:14:13 +0700

    update additional data

--- application/models/DownloadModel.php
+++ application/models/DownloadModel.php
@@ -5,6 +5,16 @@
 	{
 		parent::__construct();
 	}
+
+	function add_additional()
+	{
+		$user_id = $_SESSION['id'];
+		date_default_timezone_set('Asia/Jakarta');
+		return $data = [
+			'created_by' => $user_id,
+			'created_on' => date('Y-m-d H:i:s')
+		];
+	}
  	
  	function get(){
 		$this->db->select('id, nama, keterangan, if(is_file=1,file,link) as link_download, link, is_file, file');
@@ -18,7 +28,8 @@
  	}
 
  	function add($data){
- 		return $this->db->insert('download',$data);
+		$additional_data = $this->add_additional();
+		return $this->db->insert('download', $additional_data + $data);
  	}
  	
  	function update($id,$data){

72657d4049f48785fae63e7b54b7a5df7ae34e0f
firdausillah <firdausillah123@gmail.com>
2024-10-26 18:05:04 +0700

    update urutan download

--- application/controllers/Download.php
+++ application/controllers/Download.php
@@ -66,7 +66,7 @@ class Download extends MY_Controller
 			FROM
 				download
 			$where
-			ORDER BY download.created_on DESC
+			ORDER BY download.created_on ASC
 			LIMIT $rows_per_page
 			OFFSET $offset
 			";
